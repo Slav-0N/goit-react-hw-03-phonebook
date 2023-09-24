f25d144e8f425e13d7862901996cfb5ad7f0fb11
Slav-0N <SOVILGO@gmail.com>
2023-09-24 20:27:52 +0300

    hz

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -8,6 +8,7 @@ const ContactItems = ({ contacts, handleDelete }) => {
           <li key={el.id}>
             {el.name}: {el.number}
             <button onClick={() => handleDelete(el.id)}>Delete</button>
+            <button>TUT</button>
           </li>
         );
       })}
